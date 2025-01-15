89a778e420cf48adf35dc44de5a9a1d715d51856
Ousmane cisse <cisseousmanetanor@gmail.com>
2025-01-15 13:26:24 +0100

    Variables in java

---
+++ src/variables/Variables.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        System.out.println("============= Variables in Java =============");
+
+        /*
+            En Java, une variable est utilisée pour stocker des données qui peuvent être utilisées et manipulées dans un programme.
+            Voici les points essentiels à connaître sur les variables en Java :
+            1 - Pour déclarer une variable, vous devez spécifier son type et son nom.
+            2 - Vous pouvez assigner une valeur à une variable au moment de sa déclaration ou plus tard.
+        */
+
+        int number1 = 6;
+        int number2 = 4;
+        int result = number1 + number2;
+
+
+        System.out.println(result);
+    }
+}
